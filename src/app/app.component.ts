8ef7681d83e9d25e045cb5024446d7457ecdad6b
Steven Ontong <steven@journeyapps.com>
2023-11-21 17:20:38 -0700

    webpack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,47 @@
-import { Component, OnInit } from '@angular/core'
-import { SupabaseService } from './supabase.service'
-import { CommonModule } from '@angular/common'
-import { AuthComponent } from './auth/auth.component'
-import { ListComponent } from './list/list.component'
-import { AppSchema } from './powersync.service'
-import { WASQLitePowerSyncDatabaseOpenFactory } from '@journeyapps/powersync-sdk-web'
+import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from './supabase.service';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth/auth.component';
+import { ListComponent } from './list/list.component';
+import { AppSchema } from './powersync.service';
+import { WASQLitePowerSyncDatabaseOpenFactory } from '@journeyapps/powersync-sdk-web';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, AuthComponent, ListComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'angular-user-management'
+  title = 'angular-user-management';
 
-  session = this.supabase.session
+  session = this.supabase.session;
 
-  constructor(private readonly supabase: SupabaseService) { }
+  constructor(private readonly supabase: SupabaseService) {}
 
   ngOnInit() {
     (async () => {
       const PowerSyncFactory = new WASQLitePowerSyncDatabaseOpenFactory({
         schema: AppSchema,
         dbFilename: 'test.db'
-      })
-      const PowerSync = PowerSyncFactory.getInstance()
-      console.log(PowerSync)
+      });
+      const PowerSync = PowerSyncFactory.getInstance();
+      console.log(PowerSync);
       try {
-        PowerSync.registerListener({ 'initialized': () => console.log('initialized'), 'statusChanged': (status) => console.log(status) })
+        PowerSync.registerListener({
+          initialized: () => console.log('initialized'),
+          statusChanged: (status) => console.log(status)
+        });
         console.log('PowerSync init start');
         await PowerSync.init();
         console.log('PowerSync init start');
         await PowerSync.connect(this.supabase);
         console.log('PowerSync connect');
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-    })()
-    this.supabase.authChanges((_, session) => (this.session = session))
+    })();
+    this.supabase.authChanges((_, session) => (this.session = session));
   }
-
 }
